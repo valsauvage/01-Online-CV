9e6a5e8ffb60af689a69afd78f7b66edc2c2cb02
Val Sauvage <valencore@gmail.com>
2021-05-14 12:24:33 +0200

    Maj fullpage.js
    Mise en place du plugin fullpage.js et customisation pour correspondre a mes sections.

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: [],
+        showActiveTooltip: true,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        controlArrows:false,
+        offsetSections:true,
+        responsiveWidth: 780,
+        parallaxOptions: {
+            type:'reveal',
+            percentage: 62,
+            property:'translate'
+        },
+            
+    });
+});
