f54b108c45402993e2aacf171ec11e15d2730f46
Val Sauvage <valencore@gmail.com>
2021-05-21 11:54:00 +0200

    Maj infos CV
    Mise à jour des infos de mon CV + enlever fullpage.js + animations CSS

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-        navigation: true,
-        navigationPosition: 'right',
-        navigationTooltips: [],
-        showActiveTooltip: true,
-        slidesNavigation: true,
-        slidesNavPosition: 'bottom',
-        controlArrows:false,
-        offsetSections:true,
-        responsiveWidth: 780,
-        parallaxOptions: {
-            type:'reveal',
-            percentage: 62,
-            property:'translate'
-        },
+// $(document).ready(function() {
+//     $('#fullpage').fullpage({
+//         navigation: true,
+//         navigationPosition: 'right',
+//         navigationTooltips: [],
+//         showActiveTooltip: true,
+//         slidesNavigation: true,
+//         slidesNavPosition: 'bottom',
+//         controlArrows:false,
+//         offsetSections:true,
+//         responsiveWidth: 780,
+//         parallaxOptions: {
+//             type:'reveal',
+//             percentage: 62,
+//             property:'translate'
+//         },
             
-    });
-});
+//     });
+// });
